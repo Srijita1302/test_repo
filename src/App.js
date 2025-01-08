a891a1651f7a59925f0c084ff65d7edeb3c4405f
Srijita <srijitabhattacharyya.1302@gmail.com>
2025-01-08 16:24:52 +0530

    Update Routing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import './App.css';
 // import DashboardChart from './component/dashboard/DashboardChart';
-import Login from './component/login/Login';
+// import Login from './component/login/Login';
 import Routing from './component/routing/Routing';
 
 function App() {
